70cd619c284b14fd0daee9b800eba6e522611c46
LiRaNof <67201404+LiRaNof@users.noreply.github.com>
2021-05-01 09:07:39 +0900

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -3,3 +3,5 @@ var s = window.screen;
 var width = q.width = s.width;
 var height = q.height = s.height;
 var letters = Array(256);
+  q.getContext('2d').fillStyle='rgba(0,0,0,.05)';
+  q.getContext('2d').fillRect(0,0,width,height);
